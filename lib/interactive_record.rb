5193737820f84d3e9f82442db27286c626bce607
Christie Yoon <cleex1999@gmail.com>
2020-08-30 23:55:37 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,11 +55,11 @@ class InteractiveRecord
       DB[:conn].execute(sql, name)
    end
 
-   def self.find_by(attribute)
+   def self.find_by(row)
       #binding.pry
       sql = "SELECT * FROM #{table_name} WHERE #{column_names} = #{values_for_insert}"
-      DB[:conn].execute(sql, column_names).each do |attribute|
-        self.values_for_insert(attribute)
+      DB[:conn].execute(sql, column_names).each do |col_name|
+        self.values_for_insert(col_name)
       end
    end
 end
